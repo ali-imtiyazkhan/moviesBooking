6711c993314985383bf3af0df6bfd9eb8d4687ac
ali-imtiyazkhan <itzkhantijara@gmail.com>
2025-10-02 23:20:35 +0530

    final code 5

--- frontend/src/app/AdminLogin/page.tsx
+++ frontend/src/app/AdminLogin/page.tsx
@@ -29,8 +29,8 @@ export default function Signup() {
       alert("User Registered Successfully!");
       localStorage.setItem("token", res.data.token);
       window.location.href = "/";
-    } catch (err: any) {
-      alert(err.response?.data?.error || "Signup failed");
+    } catch (err) {
+      alert(err + "Signup failed");
     }
   };
 
--- frontend/src/app/Movies/[id]/page.tsx
+++ frontend/src/app/Movies/[id]/page.tsx
@@ -59,8 +59,8 @@ const MovieDetailPage = () => {
         }
 
         setMovie(data);
-      } catch (err: any) {
-        setError(err.message || "Something went wrong");
+      } catch (err) {
+        setError(err + "Something went wrong");
       } finally {
         setLoading(false);
       }
